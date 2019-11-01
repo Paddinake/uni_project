8ea7818627eb13147000bf891e69a136dab74247
Paddinake <p.kurz95@gmail.com>
2019-11-01 13:36:00 +0100

    static für Microsoft

--- content/overview/overview.php
+++ content/overview/overview.php
@@ -1,2 +1,48 @@
 <?php
-echo"overview.php";
+include "json_request.php";
+$result = JSONRequest();
+$result = json_decode($result, true);
+
+$key = "Meta Data";
+echo $result["Meta Data"]["1. Information"]."<br/>";
+echo $result["Meta Data"]["2. Symbol"]."<br/>";
+echo $result["Meta Data"]["3. Last Refreshed"]."<br/>";
+echo $result["Meta Data"]["4. Time Zone"]."<br/>";
+
+$dataPoints = array();
+
+foreach ($result["Weekly Time Series"] as $timestamp => $values){
+    $javaTimestamp = strtotime($timestamp)*1000;
+    array_push($dataPoints, array("x"=>$javaTimestamp, "y"=>$values["4. close"]));
+}
+
+
+
+?>
+
+<script>
+    window.onload = function () {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            animationEnabled: true,
+            zoomEnabled: true,
+            title: {
+                text: "MSFT Aktienkurs"
+            },
+            data: [{
+                type: "area",
+                xValueType: "dateTime",
+                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+            }]
+        });
+        chart.render();
+
+    }
+</script>
+
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+
+
