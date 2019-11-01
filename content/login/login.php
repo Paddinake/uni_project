bd0f4ece2056a17dbcc0ce8957e9aa2118706a92
max_mller99 <max.mller99@googlemail.com>
2019-11-01 12:15:12 +0100

    da bin ja mal gespannt ob das jetzt gepostet wird

--- content/login/login.php
+++ content/login/login.php
@@ -1,2 +1,33 @@
 <?php
-echo"login.php";
+echo"login.php";?>
+
+
+<html>
+
+<body>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <h1>Anmeldung</h1>
+    </div>
+
+    <div id= "login">
+        <form action="login.php" method="POST" style="text-align:center;">
+            <label for="Nutzername"> </label>
+            <input type="text" placeholder="Nutzername" name="Nutzername"required></br>
+
+            <label for="Passwort"> </label>
+            <input type="password" name="Passwort" placeholder="Passwort" required ></br>
+            <input type="submit" value="Login">
+
+        </form>
+
+    </div>
+    <div class="clear">
+    </div>
+    <div id="footer">
+
+    </div>
+</body>
+
+</html>
--- content/register/register.php
+++ content/register/register.php
@@ -1,2 +1,22 @@
 <?php
-echo"register.php";
+echo"register.php"; ?>
+
+<html>
+
+<form method="POST" action="index.php?content=K_L_anlegen">
+
+    <p> <b> Nutzerame </b></p>
+    <input type="text" name="Nutzername" required> <br>
+
+    <p> <b> E-Mail </b></p>
+    <input type="text" name="Anschrift" required>  <br>
+
+    <p><b> Passwort</b></p>
+    <input type="password" name="Passwort" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Mindestenst eine Zahl, ein kleiner und ein großer Buchstabe und mindestenst 8 Zeichen"  required> <br>
+
+    <input type="submit" value="Senden">
+    <input type="reset" value="Löschen">
+
+</form>
+
+</html>
