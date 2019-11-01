104ca0e2453594d477d94459ebe8bb922dd1dfac
Paddinake <p.kurz95@gmail.com>
2019-11-01 16:43:57 +0100

    Dynamisches Auswählen der Aktie

--- content/overview/overview.php
+++ content/overview/overview.php
@@ -1,48 +1,51 @@
 <?php
-include "json_request.php";
-$result = JSONRequest();
-$result = json_decode($result, true);
+include "json_requests.php";
+include "searchbar.php";
 
-$key = "Meta Data";
-echo $result["Meta Data"]["1. Information"]."<br/>";
-echo $result["Meta Data"]["2. Symbol"]."<br/>";
-echo $result["Meta Data"]["3. Last Refreshed"]."<br/>";
-echo $result["Meta Data"]["4. Time Zone"]."<br/>";
+if(isset($_GET['symbol'])) {
+    $result = JSONRequest($_GET['symbol']);
+    $result = json_decode($result, true);
 
-$dataPoints = array();
+    $key = "Meta Data";
+    echo substr($result["Meta Data"]["1. Information"],0,-37) . " (closing)<br/>";
+    echo "Symbol: ".$result["Meta Data"]["2. Symbol"] . "<br/>";
+    echo "Last Refreshed: ".$result["Meta Data"]["3. Last Refreshed"] . "<br/>";
+    echo "Timezone: ".$result["Meta Data"]["4. Time Zone"] . "<br/>";
 
-foreach ($result["Weekly Time Series"] as $timestamp => $values){
-    $javaTimestamp = strtotime($timestamp)*1000;
-    array_push($dataPoints, array("x"=>$javaTimestamp, "y"=>$values["4. close"]));
-}
+    $dataPoints = array();
 
-
-
-?>
-
-<script>
-    window.onload = function () {
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-            theme: "light2", // "light1", "light2", "dark1", "dark2"
-            animationEnabled: true,
-            zoomEnabled: true,
-            title: {
-                text: "MSFT Aktienkurs"
-            },
-            data: [{
-                type: "area",
-                xValueType: "dateTime",
-                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-            }]
-        });
-        chart.render();
-
+    foreach ($result["Weekly Time Series"] as $timestamp => $values) {
+        $javaTimestamp = strtotime($timestamp) * 1000;
+        array_push($dataPoints, array("x" => $javaTimestamp, "y" => $values["4. close"]));
     }
-</script>
+    ?>
 
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+    <script>
+        window.onload = function () {
 
+            var chart = new CanvasJS.Chart("chartContainer", {
+                theme: "light2", // "light1", "light2", "dark1", "dark2"
+                animationEnabled: true,
+                zoomEnabled: true,
+                title: {
+                    text: "Aktienkurs"
+                },
+                data: [{
+                    type: "area",
+                    xValueType: "dateTime",
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart.render();
+
+        }
+    </script>
+
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+    <?php
+} else{
+    echo "wähle eine Aktie aus";
+}
 
 
